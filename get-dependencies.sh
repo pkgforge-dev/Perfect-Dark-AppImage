9c6b6d1d66cd06c148b3cfb1177a948915e8daff
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:04:41 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,36 +6,25 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake    \
-    libdecor \
-    python   \
-    sdl2
+pacman -Syu --noconfirm cmake sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-echo "Making nightly build of Perfect Dark..."
+echo "Building Perfect Dark..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/fgsfdsfgs/perfect_dark"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-git clone "$REPO" ./perfect_dark
+git clone --depth 1 "$REPO" ./perfect_dark
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-cd ./perfect_dark
-cmake -G"Unix Makefiles" -Bbuild -DCMAKE_BUILD_TYPE=Release .
+cmake -S ./perfect_dark -G"Unix Makefiles" -Bbuild -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j$(nproc)
 
 if [ "$ARCH" = "x86_64" ]; then
-    mv -v build/pd.$ARCH ../AppDir/bin/pd
+    mv -v build/pd.$ARCH ./AppDir/bin/pd
 else
-    mv -v build/pd.arm64 ../AppDir/bin/pd
+    mv -v build/pd.arm64 ./AppDir/bin/pd
 fi
