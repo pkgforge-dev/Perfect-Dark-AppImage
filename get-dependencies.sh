c1c2c491284f7813d2eeb8145bf6eb2a3bc75de2
Link4Electronics <link4electronics@gmail.com>
2026-09-01 21:12:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,8 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake  \
-    python \
+    cmake \
     sdl2
 
 echo "Installing debloated packages..."
