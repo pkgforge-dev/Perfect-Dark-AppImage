40fc77dfa3d48467b9c171fe0da06c2498b8593b
Link4Electronics <link4electronics@gmail.com>
2026-09-10 21:44:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -22,12 +22,11 @@ git clone --depth 1 "$REPO" ./perfect_dark
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-cd ./perfect_dark
-cmake -G"Unix Makefiles" -Bbuild -DCMAKE_BUILD_TYPE=Release .
+cmake -S ./perfect_dark -G"Unix Makefiles" -Bbuild -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j$(nproc)
 
 if [ "$ARCH" = "x86_64" ]; then
-    mv -v build/pd.$ARCH ../AppDir/bin/pd
+    mv -v build/pd.$ARCH ./AppDir/bin/pd
 else
-    mv -v build/pd.arm64 ../AppDir/bin/pd
+    mv -v build/pd.arm64 ./AppDir/bin/pd
 fi
